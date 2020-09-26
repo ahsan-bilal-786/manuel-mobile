00e6ca75bcc5555d7ae99d71b2b6efa86c870c80
ahsanbilalas <engr.ahsan.bilal@gmail.com>
2020-09-26 21:32:21 +0500

    integrate user form validation and user verification api via verifiation code

--- src/screens/Auth/VerifyAccount.js
+++ src/screens/Auth/VerifyAccount.js
@@ -1,28 +1,79 @@
-import * as React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Text, Input, Button} from 'react-native-elements';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {styles, textStyles} from 'screens/Auth/styles';
+import {useFormik} from 'formik';
+import * as Yup from 'yup';
+import get from 'lodash/get';
+import {verifyUserAccount, registerToken} from 'api';
+import {getUserToken} from 'utils/asyncStorage';
+
+const initialValues = {
+  verificationCode: '',
+};
+
+const validationSchema = Yup.object().shape({
+  verificationCode: Yup.string().required(
+    'Please enter the token sent to your Email.',
+  ),
+});
 
 const VerifyAccountScreen = ({navigation}) => {
+  const formik = useFormik({
+    initialValues,
+    validationSchema,
+    onSubmit: (values, {setSubmitting, setErrors}) => {
+      const {verificationCode} = values;
+      verifyUserAccount(verificationCode)
+        .then((resp) => {
+          setSubmitting(false);
+          navigation.navigate('Profile');
+        })
+        .catch((e) => {
+          setSubmitting(false);
+          if (get(e, 'response.data.errors')) {
+            setErrors(e.response.data.errors);
+          }
+        });
+    },
+  });
+  useEffect(() => {
+    getUserToken().then((resp) => {
+      registerToken(resp);
+    });
+  }, []);
+
+  const {
+    values,
+    touched,
+    errors,
+    handleChange,
+    setFieldTouched,
+    isSubmitting,
+    handleSubmit,
+  } = formik;
   return (
     <SafeAreaView style={[styles.wrapper, styles.alignTop]}>
       <Text h1 style={styles.heading}>
         Fiverr.
       </Text>
-
       <Input
         placeholder="Verification Code"
-        errorMessage=""
         placeholderTextColor={textStyles.color}
         inputContainerStyle={textStyles}
         errorStyle={textStyles}
         style={textStyles}
+        value={values.verificationCode}
+        errorMessage={touched.verificationCode && errors.verificationCode}
+        onChangeText={handleChange('verificationCode')}
+        onBlur={() => setFieldTouched('verificationCode')}
       />
 
       <Button
         title="Verify"
         containerStyle={styles.submitBtn}
-        onPress={() => navigation.navigate('PhotoUpload')}
+        disabled={isSubmitting}
+        onPress={handleSubmit}
       />
     </SafeAreaView>
   );
